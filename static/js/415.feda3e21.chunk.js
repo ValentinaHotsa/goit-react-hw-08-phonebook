d668f2cd07773d61c2a3ad9aff6132093081d1be
ValentinaHotsa <134721626+ValentinaHotsa@users.noreply.github.com>
2024-01-25 11:30:22 +0000

    Deploying to gh-pages from @ ValentinaHotsa/goit-react-hw-08-phonebook@d46352f3d96618b6af4629fd6fb662a0356e1dd0 🚀

---
+++ static/js/415.feda3e21.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,o,n){n.r(o),n.d(o,{default:function(){return t}});var r=n(184);function t(){return(0,r.jsxs)("div",{children:[(0,r.jsx)("h1",{children:"PhoneBook welcome page "}),(0,r.jsx)("p",{children:"Create your own personal book of contacts"})]})}}}]);
+//# sourceMappingURL=415.feda3e21.chunk.js.map
